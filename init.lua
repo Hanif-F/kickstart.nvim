53d1e0020d58bd7b6c008176e4565040d8339c98
Hanif-F <haniffaris159@gmail.com>
2024-05-18 09:49:13 +0700

    autoclose

--- init.lua
+++ init.lua
@@ -172,6 +172,37 @@ require('lazy').setup({
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
+  -- autoclosing brackets, quote, etc
+  {
+    'm4xshen/autoclose.nvim',
+    config = function()
+      require('autoclose').setup {
+        keys = {
+          ['('] = { escape = false, close = true, pair = '()' },
+          ['['] = { escape = false, close = true, pair = '[]' },
+          ['{'] = { escape = false, close = true, pair = '{}' },
+
+          ['>'] = { escape = true, close = false, pair = '<>' },
+          [')'] = { escape = true, close = false, pair = '()' },
+          [']'] = { escape = true, close = false, pair = '[]' },
+          ['}'] = { escape = true, close = false, pair = '{}' },
+
+          ['"'] = { escape = true, close = true, pair = '""' },
+          ["'"] = { escape = true, close = true, pair = "''" },
+          ['`'] = { escape = true, close = true, pair = '``' },
+        },
+        options = {
+          disabled_filetypes = { 'text' },
+          disable_when_touch = false,
+          touch_regex = '[%w(%[%{]', -- Ensure `{` is escaped correctly
+          pair_spaces = false,
+          auto_indent = true,
+          disable_command_mode = false,
+        },
+      }
+    end,
+  },
+
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
