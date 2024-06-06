15afef7447c76d1ea7f8590e53f8b3200184ac6b
Hanif-F <haniffaris159@gmail.com>
2024-06-06 17:32:31 +0700

    Remove go.nvim
    Remove go.nvim due to incompatibilities in config, might fix later

--- init.lua
+++ init.lua
@@ -312,21 +312,6 @@ require('lazy').setup({
     },
   },
 
-  {
-    'ray-x/go.nvim',
-    dependencies = { -- optional packages
-      'ray-x/guihua.lua',
-      'neovim/nvim-lspconfig',
-      'nvim-treesitter/nvim-treesitter',
-    },
-    config = function()
-      require('go').setup()
-    end,
-    event = { 'CmdlineEnter' },
-    ft = { 'go', 'gomod' },
-    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-  },
-
   -- Troubleshooting plugin
   {
     'folke/trouble.nvim',
